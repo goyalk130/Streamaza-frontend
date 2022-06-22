e6324b4a7783a6ff63b567f7c222e5f4cab98f82
goyalk130 <goyalkaran130@gmail.com>
2022-06-22 23:46:54 +0530

    d

---
+++ build/precache-manifest.ff32095ea5d110dbc99156e31da2ebd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d73c382643cb9fcf1ecc",
+    "url": "/static/css/main.31dddb1b.chunk.css"
+  },
+  {
+    "revision": "d73c382643cb9fcf1ecc",
+    "url": "/static/js/main.d73c3826.chunk.js"
+  },
+  {
+    "revision": "f5dedfa180ef8b24f113",
+    "url": "/static/js/1.f5dedfa1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5ed5477013d3eb905f41d620eaa72976",
+    "url": "/index.html"
+  }
+];
